662e2cdf1c027b6aba9c96fd46e04aad8e7a34dc
himani-s993 <himanis993@gmail.com>
2023-01-10 22:53:22 +0530

    Update TC

--- docker-compose-spring-boot-stock/stock-app/src/test/java/com/stockapp/spring/datajpa/SpringBootApplicationIntergrationTest.java
+++ docker-compose-spring-boot-stock/stock-app/src/test/java/com/stockapp/spring/datajpa/SpringBootApplicationIntergrationTest.java
@@ -47,48 +47,6 @@ public class SpringBootApplicationIntergrationTest {
 	}
 
 	@Test
-	public void givenStocks_whenAddStocks_thenStatus204() throws Exception {
-		// given - setup or precondition
-		Stock stock = new Stock("ICICI", 200);
-		stock = stockRepository.save(stock);
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-		String requestJson = ow.writeValueAsString(stock);
-
-		// when - action
-		ResultActions response = mockMvc.perform(
-				MockMvcRequestBuilders.post("/api/stocks").contentType(APPLICATION_JSON_UTF8).content(requestJson));
-
-		response.andExpect(MockMvcResultMatchers.status().isNoContent());
-		// String output = response.andReturn().getResponse().getContentAsString();
-
-		// cleanup
-		stockRepository.delete(stock);
-	}
-
-	@Test
-	public void givenStocks_whenAddStocks_thenStatus201() throws Exception {
-		// given - setup or precondition
-		Stock stock = new Stock("ICICI", 200);
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-		String requestJson = ow.writeValueAsString(stock);
-
-		// when - action
-		ResultActions response = mockMvc.perform(
-				MockMvcRequestBuilders.post("/api/stocks").contentType(APPLICATION_JSON_UTF8).content(requestJson));
-
-		response.andExpect(MockMvcResultMatchers.status().isCreated());
-		String output = response.andReturn().getResponse().getContentAsString();
-		Stock outputStock = mapper.readValue(output, Stock.class);
-
-		// cleanup
-		stockRepository.delete(outputStock);
-	}
-
-	@Test
 	public void givenStocks_whenGetStockById_thenStatus200() throws Exception {
 		// given - setup or precondition
 		Stock stock = new Stock("SEC", 200);
